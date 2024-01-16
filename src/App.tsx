9ad48643329378dfe11926caa7a57aa86fe050e3
Yozamu <yozamu@hotmail.fr>
2024-01-16 16:09:13 +0100

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Button onClick={() => alert('ok')}>text</Button>
+    </>
+  );
+}
+
+export default App;
